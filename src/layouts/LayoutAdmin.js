df63fac783cc0eee769bca65d273a71dceefe8a3
Luka Brizzi <lukabrizzi@MacBook-Air-de-Luka.local>
2021-05-29 11:35:18 -0300

    Casi terminado

--- src/layouts/LayoutAdmin.js
+++ src/layouts/LayoutAdmin.js
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
-import { Layout, Menu, Card, Col, Row, Avatar, Carousel } from 'antd';
+import { Layout, Card, Col, Row, Avatar, Carousel } from 'antd';
 import 'antd/dist/antd.css';
 import { DownloadOutlined, TwitterOutlined, InstagramOutlined, LinkedinOutlined, WhatsAppOutlined, CodeOutlined } from '@ant-design/icons';
 
-import Logo from '../assets/img/png/flecaPunto.png'
 import header from '../assets/img/png/presentacion.png'
 
 // import cvImg from '../../assets/img/png/cv.png'
@@ -20,20 +19,10 @@ import spotify from '../assets/img/png/spotify.png'
 import './LayoutAdmin.scss'
 
 export default function LayoutAdmin(props) {
-    const { routes } = props;
-    const { Header, Content, Footer } = Layout;
+    const { Content, Footer } = Layout;
     const { Meta } = Card;
 
     const avatarColor = '#25d366';
-
-    const contentStyle = {
-        // height: '10px',
-        color: '#fff',
-        lineHeight: '300px',
-        textAlign: 'center',
-        background: '#364d79',
-    };
-
 
     function Instagram() {
         window.open("https://www.instagram.com/lukabrizzi");
@@ -44,7 +33,7 @@ export default function LayoutAdmin(props) {
     }
 
     function LinkedIn() {
-        window.open("https://www.linkedin.com/in/luka-brizzi-b19b971a8/");
+        window.open("https://www.linkedin.com/in/luka-brizzi/");
     }
 
     function WhatsApp() {
@@ -67,145 +56,171 @@ export default function LayoutAdmin(props) {
         window.open("https://www.instagram.com/ferreteria_elgringo");
     }
 
+    function Spotify() {
+        window.open("https://open.spotify.com/user/luukab08");
+    }
+
+    function FadeInSection(props) {
+        const [isVisible, setVisible] = React.useState(false);
+        const domRef = React.useRef();
+        React.useEffect(() => {
+            const observer = new IntersectionObserver(entries => {
+                entries.forEach(entry => setVisible(entry.isIntersecting));
+            });
+            observer.observe(domRef.current);
+        }, []);
+        return (
+            <div
+                className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+                ref={domRef}
+            >
+                {props.children}
+            </div>
+        );
+    }
+
     return (
         <Layout>
             <Content
                 className="layoutAdmin__content"
-                style={{ paddingLeft: 40, paddingRight: 40 }}
+                style={{ paddingLeft: 30, paddingRight: 30 }}
             >
                 <Content
                     className="layoutAdmin__content"
-                    style={{ paddingLeft: 40, paddingRight: 40 }}
+                    style={{ paddingLeft: 10, paddingRight: 10 }}
                 >
-                    <div class="transition">
+                    <FadeInSection key={1}>
                         <div className="layoutAdmin__header" >
                             <img
                                 src={header}
                                 alt="Luka Brizzi"
                             />
                         </div >
-                    </div>
+                    </FadeInSection>
                 </Content>
-                <Row gutter={16}>
-                    <Col span={12}>
-                        <Card
-                            cover={
-                                <img
-                                    alt="example"
-                                    src={cvImg}
+                <FadeInSection key={2}>
+                    <Row gutter={16}>
+                        <Col span={12}>
+                            <Card
+                                cover={
+                                    <img
+                                        alt="example"
+                                        src={cvImg}
+                                    />
+                                }
+                                actions={[
+                                    <Link to={cvFileEsp} target="_blank" download><DownloadOutlined /> Spanish</Link>,
+                                    <Link to={cvFileIng} target="_blank" download><DownloadOutlined /> English</Link>
+                                ]}
+                            >
+                                <Meta
+                                    title="Mi Cv"
+                                    description=""
                                 />
-                            }
-                            actions={[
-                                <Link to={cvFileEsp} target="_blank" download><DownloadOutlined /> Spanish</Link>,
-                                <Link to={cvFileIng} target="_blank" download><DownloadOutlined /> English</Link>
-                            ]}
-                        >
-                            <Meta
-                                title="Mi Cv"
-                                description=""
-                            />
-                        </Card>
-                    </Col>
+                            </Card>
+                        </Col>
 
-                    <Col span={12}>
-                        <Card
-                            cover={
-                                <img
-                                    alt="example"
-                                    src={imgSillon}
+                        <Col span={12}>
+                            <Card
+                                cover={
+                                    <img
+                                        alt="example"
+                                        src={imgSillon}
+                                    />
+                                }
+                                actions={[
+                                    <Link onClick={Instagram}><InstagramOutlined /> Instagram</Link>,
+                                    <Link onClick={Twitter}><TwitterOutlined /> Twitter</Link>,
+                                    <Link onClick={LinkedIn}><LinkedinOutlined /> LinkedIn</Link>
+                                ]}
+                            >
+                                <Meta
+                                    title="Conocé un poco más de mi en mis redes sociales"
+                                    description=""
                                 />
-                            }
-                            actions={[
-                                <Link onClick={Instagram}><InstagramOutlined /> Instagram</Link>,
-                                <Link onClick={Twitter}><TwitterOutlined /> Twitter</Link>,
-                                <Link onClick={LinkedIn}><LinkedinOutlined /> LinkedIn</Link>
-                            ]}
-                        >
-                            <Meta
-                                title="Conocé un poco más de mi en mis redes sociales"
-                                description=""
-                            />
-                        </Card>
-                    </Col>
-                </Row>
+                            </Card>
+                        </Col>
+                    </Row>
+                </FadeInSection>
             </Content>
 
             <Content
                 className="layoutAdmin__content"
-                style={{ paddingLeft: 40, paddingRight: 40 }}
+                style={{ paddingLeft: 30, paddingRight: 30 }}
             >
-                <Row gutter={16}>
-                    <Col span={12}>
-                        <Card
-                            cover={
-                                <img
-                                    alt="example"
-                                    src={kerde}
+                <FadeInSection key={3}>
+                    <Row gutter={16}>
+                        <Col span={12}>
+                            <Card
+                                cover={
+                                    <img
+                                        alt="example"
+                                        src={kerde}
+                                    />
+                                }
+                                actions={[
+                                    <Link onClick={Kerde}>Kerde Labs <CodeOutlined /></Link>
+                                ]}
+                            >
+                                <Meta
+                                    title="Trabajo actual de desarrollo"
+                                    description="Kerde Labs"
                                 />
-                            }
-                            actions={[
-                                <Link onClick={Kerde}>Kerde Labs <CodeOutlined /></Link>
-                            ]}
-                        >
-                            <Meta
-                                title="Trabajo actual de desarrollo"
-                                description="Kerde Labs"
-                            />
-                        </Card>
-                    </Col>
+                            </Card>
+                        </Col>
 
-                    <Col span={12}>
-                        <Card
-                            cover={
-                                <img
-                                    alt="example"
-                                    src={marketing}
+                        <Col span={12}>
+                            <Card
+                                cover={
+                                    <img
+                                        alt="example"
+                                        src={marketing}
+                                    />
+                                }
+                                actions={[
+                                    <Link onClick={Dijon}><InstagramOutlined /> Dijon</Link>,
+                                    <Link onClick={American}><InstagramOutlined /> American Cars</Link>,
+                                    <Link onClick={Ferre}><InstagramOutlined /> EG Ferreteria</Link>
+                                ]}
+                            >
+                                <Meta
+                                    title="Trabajo actual de marketing y diseño grafico"
+                                    description="Dijon - American Cars - EG Ferreteria"
                                 />
-                            }
-                            actions={[
-                                <Link onClick={Dijon}><InstagramOutlined /> Dijon</Link>,
-                                <Link onClick={American}><InstagramOutlined /> American Cars</Link>,
-                                <Link onClick={Ferre}><InstagramOutlined /> EG Ferreteria</Link>
-                            ]}
-                        >
-                            <Meta
-                                title="Trabajo actual de marketing y diseño grafico"
-                                description="Dijon - American Cars - EG Ferreteria"
-                            />
-                        </Card>
-                    </Col>
-                </Row>
+                            </Card>
+                        </Col>
+                    </Row>
+                </FadeInSection>
             </Content>
-            <Carousel autoplay>
-                <div>
-                    <img
-                        className="layoutAdmin__carrusel"
-                        alt="example"
-                        src={spotify}
-                    />
-                </div>
-                <div>
-                    <img
-                        className="layoutAdmin__carrusel"
-                        alt="example"
-                        src={spotify}
-                    />
-                </div>
-                <div>
-                    <img
-                        className="layoutAdmin__carrusel"
-                        alt="example"
-                        src={spotify}
-                    />
-                </div>
-                <div>
-                    <img
-                        className="layoutAdmin__carrusel"
-                        alt="example"
-                        src={spotify}
-                    />
-                </div>
-            </Carousel>
+            <FadeInSection key={4}>
+                <Carousel autoplay>
+                    <div className="carrusel">
+                        <img
+                            onClick={Spotify}
+                            alt="example"
+                            src={spotify}
+                        />
+                    </div>
+                    <div className="carrusel">
+                        <img
+                            alt="example"
+                            src={spotify}
+                        />
+                    </div>
+                    <div className="carrusel">
+                        <img
+                            alt="example"
+                            src={spotify}
+                        />
+                    </div>
+                    <div className="carrusel">
+                        <img
+                            alt="example"
+                            src={spotify}
+                        />
+                    </div>
+                </Carousel>
+            </FadeInSection>
             <Footer className="layoutAdmin__footer" style={{ textAlign: 'center' }}>Made with React ©2021 Created by Luka Brizzi
                 <div className="layoutAdmin__wpp" style={{ textAlign: 'right' }}>
                     <Avatar onClick={WhatsApp}
